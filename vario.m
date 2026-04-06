86b44c8367c887e836562f532d1b9776c76d03f2
Patrick Rapin <patrick@airnavigation.aero>
2026-04-06 09:55:16 +0200

    2025-09-05: generating an HTML email

--- vario.m
+++ vario.m
@@ -11,12 +11,11 @@ print([date '.png']);
 
 f = fopen([date '.txt'], 'wt');
 vario=data(:,2);
-fprintf(f, 'Rapport VARIO du %s\n\n', date)
 [a, b] = min(vario);
-fprintf(f, 'Prix minimal %.2f ct/kWh à %s\n', a, hour((b - 1)/4));
+fprintf(f, '%.2f %s ', a, hour((b - 1)/4));
 [a, b] = max(vario);
-fprintf(f, 'Prix maximal %.2f ct/kWh à %s\n', a, hour((b - 1)/4));
-fprintf(f, 'Prix moyen   %.2f ct/kWh\n', mean(vario));
+fprintf(f, '%.2f %s ', a, hour((b - 1)/4));
+fprintf(f, '%.2f\n', mean(vario));
 
 plage=13; # 3h
 c=conv(vario, ones(plage, 1), 'valid');
@@ -24,9 +23,8 @@ d=diff(sign(diff(c)));
 mins = find(d > 0);
 h = [data(mins+7-6, 1) data(mins+7+6, 1)];
 p=c(mins+1)/plage;
-fprintf(f, '\nMeilleures plages de 3h:\n');
 for i=1:numel(p)
-	fprintf(f, 'De %s à %s: %.2f ct/kWh\n', hour(h(i, 1)), hour(h(i, 2)), p(i));
+	fprintf(f, '%s %s %.2f\n', hour(h(i, 1)), hour(h(i, 2)), p(i));
 end
 fclose(f);
 
--- vario_check.pl
+++ vario_check.pl
@@ -2,7 +2,8 @@
 use strict;
 use JSON;
 use Data::Dumper;
-
+ use MIME::Base64;
+ 
 my $date = $ARGV[0];
 my $data = `curl -s "https://www.groupe-e.ch/fr/api/vario/${date}T"` or die;
 my $json = decode_json($data);
@@ -19,7 +20,58 @@ for my $i(@data)
 	else 
 		{ die; }
 }
-
+close OUT;
 system "octave --eval \"vario('$date')\" 2>/dev/null";
-system "mail -s 'Vario $date' -A $date.txt -A $date.png patrick\@airnavigation.aero  < $date.txt"
-#print Dumper(\@data);
+
+open IN, "$date.txt" or die;
+$_ = <IN>;
+my @a = split;
+open OUT, ">$date.mail";
+print OUT << "_END_";
+--boundary-separator
+Content-Type: text/html; charset="utf-8"
+
+<html>
+<head><title>Rapport Vario du $date</title></head>
+<body>
+<h1>Rapport Vario du $date</h1>
+
+<table>
+<tr><td>Prix minimal</td><td><b>$a[0]</b> ct / kWh</td><td>à <b>$a[1]</b></td></tr>
+<tr><td>Prix maximal</td><td><b>$a[2]</b> ct / kWh</td><td>à <b>$a[3]</b></td></tr>
+<tr><td>Prix moyen</td><td><b>$a[4]</b> ct / kWh</td></tr>
+</table>
+
+<br>
+<table>
+_END_
+
+while(<IN>)
+{
+	@a = split;
+	print OUT "<tr><td>De <b>$a[0]</b></td><td>à <b>$a[1]</b>:</td><td>moyenne <b>$a[2]</b> ct / kWh</td></tr>\n";
+}
+print OUT << "_END_";
+</table> <br>
+<IMG SRC="cid:airnavigation.aero" ALT="graphique">
+</body>
+</html>
+
+--boundary-separator
+Content-Location: CID:somethingatelse ; this header is disregarded
+Content-ID: <airnavigation.aero>
+Content-Type: IMAGE/PNG
+Content-Transfer-Encoding: BASE64
+
+_END_
+undef $/;
+open IN, "$date.png";
+$_ = <IN>;
+print OUT encode_base64($_);
+
+print OUT "\n--boundary-separator--\n";
+
+my $email = 'patrick@airnavigation.aero';
+#my $email = 'rapin.patrick@gmail.com';
+
+system "cat $date.mail  | mail -s 'Rapport Vario du $date' $email --content-type='multipart/related;boundary=\"boundary-separator\";type=\"text/html\"'";
